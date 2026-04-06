02ee47c7f9d3b03f8650957a039cdbee3ee315db
Maxm2200 <kislov.okisheh@gmail.com>
2026-04-06 13:20:04 +0700

    pls 2.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
-        ...globals.node,
-      },
+        ...globals.node
+      }
     },
     rules: {
-      semi: ['error', 'never'],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'eol-last': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-      'no-trailing-spaces': 'error',
-      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-    },
-  },
-])
+      semi: ["error", "never"],
+      quotes: ["error", "single", { avoidEscape: true }],
+      "eol-last": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "brace-style": ["error", "1tbs", { allowSingleLine: true }],
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }]
+    }
+  }
+]);
